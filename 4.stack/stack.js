73d65e543f226c32aa4a7d7411776494e74f5254
Maiduo <wtd@choicesoft.com.cn>
2018-11-07 10:23:45 +0800

    rewrite stack use ES6

--- 4.stack/stack.js
+++ 4.stack/stack.js
@@ -1,28 +1,38 @@
-function Stack(){
-  this.dataStore = [];
-  this.top = 0;
-  this.push = push;
-  this.pop = pop;
-  this.peek = peek;
-  this.length = length;
-  this.clear = clear;
+class Stack {
+  constructor() {
+    this.dataStore = [];
+    this._top = 0; // 栈顶元素是栈中第几个,也代表栈的 长度
+  }
+
+  // 向栈中压入一个元素
+  push(element) {
+    this.dataStore[this._top ++] = element;
+  }
+
+  // 从栈顶弹出一个元素，并作为返回值
+  pop() {
+    if (this._top <= 0) return;
+    return this.dataStore[-- this._top];
+  }
+
+  //返回栈第top个位置（栈顶）的元素,不会改变栈的长度
+  peek() {
+    return this.dataStore[this._top - 1]
+  }
+
+  length() {
+    return this._top;
+  }
+
+  clear() {
+    delete this.dataStore;
+    this.dataStore = [];
+    this._top = 0;
+  }
 }
 
-//向栈中压入一个元素
-function push(element){
-  this.dataStore[this.top++] = element;
-}
-//从栈中弹出一个元素，并作为返回值(返回的值也即弹出的元素)
-function pop(){
-  return this.dataStore[--this.top];//top=数组下标+1，（有一个元素dataStore[0]时top=1）
-}
-//返回数组第top-1个位置的元素
-function peek(){
-  return this.dataStore[this.top-1];
-}
-function length(){
-  return this.top;
-}
-function clear(){
-  this.top = 0;
-}
+const stack = new Stack();
+const top = stack.pop();
+console.log(top) // undefined
+stack.push('MAiduo');
+console.log(stack)
